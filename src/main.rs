7d305d05c38c7d35cc582d3e7de9184eb21fbfb9
not-human213 <harshalbhavsar213@gmail.com>
2026-05-24 18:33:46 +0530

    CPU goes brrrr:

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::{thread, time::Duration};
 use sysinfo::{Components, Disks, Networks, System};
 
 fn main() {
@@ -6,7 +5,8 @@ fn main() {
     let mut networks = Networks::new_with_refreshed_list();
     let mut disks = Disks::new_with_refreshed_list();
     let mut components = Components::new_with_refreshed_list();
-
+    println!("System name {:?}",System::name());
+    println!("CPUs {}", sys.cpus().len());
     loop{
         sys.refresh_all();
         networks.refresh(true);
@@ -14,5 +14,24 @@ fn main() {
         components.refresh(true);
 
         println!("Memory: {} MB / {} MB", sys.used_memory() /1024/1024, sys.total_memory() /1024/1024);
+
+        println!("CPU usage: {}%", sys.global_cpu_usage());
+        println!("CPU frequency: {} MHz", sys.cpus()[0].frequency());
+        let (temperature_sum, temperature_count) = components
+            .iter()
+            .filter_map(|component| component.temperature())
+            .fold((0.0_f32, 0_usize), |(sum, count), temperature| {
+                (sum + temperature, count + 1)
+            });
+
+        if temperature_count > 0 {
+            println!(
+                "CPU temperature: {:.1}°C",
+                temperature_sum / temperature_count as f32
+            );
+        } else {
+            println!("CPU temperature: unavailable");
+        }
+
     }
 }
